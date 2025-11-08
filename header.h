09c9e27145b44d23dbe371f2cac36149ebbbfdf6
boubou120 <bouazj93@gmail.com>
2025-11-08 23:51:16 +0100

    test

--- header.h
+++ header.h
@@ -22,14 +22,14 @@
 #define LINE_CONFIG 1
 #define LINE_MAP 2
 
-# define KEY_W 119
-# define KEY_A 97
-# define KEY_S 115
-# define KEY_D 100
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
 
-# define KEY_ESC 65307
-# define KEY_LEFT 65361
-# define KEY_RIGHT 65363
+#define KEY_ESC 65307
+#define KEY_LEFT 65361
+#define KEY_RIGHT 65363
 
 
 typedef struct s_img
